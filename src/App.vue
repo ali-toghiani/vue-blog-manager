7635ba62834cacadb76211c8af1eff638d5fa5f6
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-05-29 14:45:19 +0330

    feat(Vue.vue): set box-sizing globally

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <h1 class="text-red-500 h-[100vh]">asdasdf</h1>
-  </nav>
   <router-view/>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+* {
+  box-sizing: border-box;
 }
 
 nav {
