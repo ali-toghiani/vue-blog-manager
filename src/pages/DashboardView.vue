b91281775f8bb4fec21c3015e067a3c26750c598
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-05-30 15:34:23 +0330

    feat(DashboardView.vue):  add sidebar & header

--- src/pages/DashboardView.vue
+++ src/pages/DashboardView.vue
@@ -1,5 +1,22 @@
 <template>
-  <h1>Dashboard</h1>
-
-  <router-view></router-view>
+  <app-header></app-header>
+  <div class="contenct-container flex">
+    <app-sidebar></app-sidebar>
+    <div class="router-view-container md:p-6 bg-gray-300 w-full">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
+
+<script>
+import AppHeader from '@/layout/AppHeader.vue';
+import AppSidebar from '@/layout/AppSidebar.vue';
+
+export default {
+  name: 'app-dashaobrd-view',
+  components: {
+    AppHeader,
+    AppSidebar
+  }
+}
+</script> 
