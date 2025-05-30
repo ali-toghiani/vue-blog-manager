bddfbc1299e4a02ed5de1aeeb72c78f8a7a76e64
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-05-30 13:50:32 +0330

    feat(index.js): save user in localstorage and vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,31 @@ import { createStore } from 'vuex'
 export default createStore({
   state: () => ({
     baseApi : process.env.VUE_APP_BASE_API_URL,
+    user: null
   }),
   getters: {
     baseApi(state){
       return state.baseApi;
+    },
+    token(state) {
+      if (!state.user){
+        const localUserRaw = localStorage.getItem('user');
+        const localUser = JSON.parse(localUserRaw);
+        if (localUser) {
+          state.user = localUser;
+        }
+      }
+      return state.user.token;
+    },
+    userName(state){
+      return state.user.username;
     }
   },
   mutations: {
+    setUser(context, payload){
+      localStorage.setItem('user', JSON.stringify(payload));
+      context.user = payload;
+    }
   },
   actions: {
   },
