80a3782068b4a1eb21f7f647a9eee1c561698951
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-05-31 19:07:11 +0330

    feat(main.js): config toastify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './assets/tailwind.css'
-import './assets/fonts.css'
+import { createApp } from 'vue';
+import Vue3Toastify from 'vue3-toastify';
 
-createApp(App).use(store).use(router).mount('#app')
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+// Styles Imports
+import './assets/tailwind.css';
+import './assets/fonts.css';
+import 'vue3-toastify/dist/index.css';
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Vue3Toastify, {
+    autoClose: 3000,
+    position: 'top-center',
+    hideProgressBar: true,
+    icon: false,
+    closeButton: false,
+  })
+  .mount('#app');
