dc16c301d62ebdb2af61a0d0b5d0af235250310d
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-05-31 09:29:41 +0330

    feat(AppWidget.vue): create widget component

---
+++ src/components/AppWidget.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="bg-white rounded-md">
+    <h2 class="text-start text-[18px] font-semibold ps-6 py-[38px]">
+      {{ title }}
+    </h2>
+    <hr />
+
+    <slot>
+      <app-replacer></app-replacer>
+    </slot>
+  </main>
+</template>
+
+<script>
+  import AppReplacer from './AppReplacer.vue';
+
+  export default {
+    name: 'app-widget',
+    components: {
+      AppReplacer,
+    },
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+</script>
