3bcff6e8e7ef14256431b1356b813f01c15dfeca
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-06-08 18:59:34 +0330

    feat(NotFound.vue): add back button to redirect user to articles

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,21 @@
 <template>
-<h1>404 Not Found</h1>
+  <div
+    class="main-container h-[100dvh] w-full flex flex-col justify-center items-center"
+  >
+    <warning-icon class="fill-golden-state w-[60px] h-[60px] mb-4" />
+    <h1 class="text-[45px] font-bold text-golden-state">404 Not Found</h1>
+    <app-link-button path="/articles">Go Back</app-link-button>
+  </div>
 </template>
+
+<script>
+  import WarningIcon from '@/assets/icons/WarningIcon.vue';
+  import AppLinkButton from '@/components/AppLinkButton.vue';
+
+  export default {
+    components: {
+      AppLinkButton,
+      WarningIcon,
+    },
+  };
+</script>
